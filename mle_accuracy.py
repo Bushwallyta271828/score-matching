20fe4174d826932c68157fb138c04eafe20093a0
Bushwallyta271828 <bushwallyta@gmail.com>
2022-06-22 20:20:28 -0400

    Updated mle_accuracy.py

--- mle_accuracy.py
+++ mle_accuracy.py
@@ -3,40 +3,44 @@ from scipy.optimize import minimize
 from scipy.integrate import quad
 import sufficient_statistics
 import sample
+import test_class
 #import matplotlib.pyplot as plt
 
 
-def non_normalized_pdf(x, thetas):
-    return np.exp(sufficient_statistics.sufficient_statistics(np.array([x]), thetas))
+def non_normalized_pdf(x, thetas, suffstats):
+    suff_stat_values = sufficient_statistics.zeroth_derivatives(suffstats, np.array([x]))
+    exponent = np.dot(thetas, suff_stat_values)[0]
+    return np.exp(exponent)
 
 
-def normalize(thetas):
+def normalize(thetas, suffstats):
     #compute the partition function
-    return quad(non_normalized_pdf, -np.infty, np.infty, args=(thetas,))[0]
+    return quad(non_normalized_pdf, -np.infty, np.infty, args=(thetas, suffstats))[0]
 
 
-def negative_log_likelihood(thetas, xs):
-    #assert all thetas are positive:
+def negative_log_likelihood(thetas, suffstats, xs):
     if np.any(thetas <= 0):
         print("WARNING: theta <= 0")
         #Note: sometimes minimize passes a theta with a negative element.
         return 10**6 * (-np.min(thetas) + 1) #gradient should push in right direction
-    Z = normalize(thetas)
-    log_likelihood = np.sum(sufficient_statistics.sufficient_statistics(xs, thetas) - np.log(Z))
+    Z = normalize(thetas, suffstats)
+    suff_stat_values = sufficient_statistics.zeroth_derivatives(suffstats, xs)
+    non_normalized_exponents = np.dot(thetas, suff_stat_values)
+    log_likelihood = np.sum(non_normalized_exponents - np.log(Z))
     return -log_likelihood
 
 
-def mle_accuracy(n, theta_star, initial_theta):
-    #assert all thetas are positive:
-    if np.any(theta_star <= 0):
-        raise ValueError("theta_star <= 0")
-    if np.any(initial_theta <= 0):
-        raise ValueError("initial_theta <= 0")
-    #find the samples:
-    samples = sample.sample(theta_star, n)
+def mle_accuracy(test_parameters):
+    #test_parameters is of type test_class.TestParameters
+    assert(test_parameters.method == "mle")
+    assert(np.all(test_parameters.theta_star >= 0))
+    samples = sample.sample(test_parameters)
     #find the MLE with theta bounded to be positive:
-    bounds = [(0, None) for i in range(np.size(theta_star))]
-    MLE_answer = minimize(negative_log_likelihood, initial_theta, args=(samples,), bounds=bounds).x
+    bounds = [(0, None) for i in range(np.size(test_parameters.theta_star))]
+    MLE_answer = minimize(negative_log_likelihood,
+                            test_parameters.theta_star,
+                            args=(test_parameters.suffstats, samples),
+                            bounds=bounds).x
     dist = np.sqrt(np.sum((MLE_answer - theta_star)**2))
     return (MLE_answer, dist)
 
