1a3d82699781bdec22574cf4e2db363cf8f0288c
Bushwallyta271828 <bushwallyta@gmail.com>
2022-06-22 20:35:00 -0400

    Fixed bug in scorematching_accuracy.py

--- scorematching_accuracy.py
+++ scorematching_accuracy.py
@@ -9,9 +9,8 @@ def scorematching(test_parameters):
     #test is of type test_class.TestParameters
 
     #assert all thetas are positive:
-    if np.any(test_parameters.theta_star <= 0):
-        raise ValueError("theta_star <= 0")
-    samples = sample.sample(test_parameters.theta_star, test_parameters.n)
+    assert(np.all(test_parameters.theta >= 0))
+    samples = sample.sample(test_parameters)
 
     first = sufficient_statistics.first_derivatives(test_parameters.suffstats, samples).T
     matrix_term_of_x = first[:,:,np.newaxis] * first[:,np.newaxis,:]
@@ -20,6 +19,6 @@ def scorematching(test_parameters):
     vector_term = np.average(second, axis=0)
 
     scorematch_answer = -np.dot(inv(matrix_term), vector_term)
-    dist = np.sqrt(np.sum((scorematch_answer - theta_star)**2))
+    dist = np.sqrt(np.sum((scorematch_answer - test_parameters.theta_star)**2))
     return (scorematch_answer, dist)
 
