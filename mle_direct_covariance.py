3b055e7c6055eae207f2a9f68ddc6a264eb7015d
Alexander Heckett <aheckett@gmail.com>
2022-06-30 21:17:46 -0400

    created mle_direct_covariance.py

---
+++ mle_direct_covariance.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sufficient_statistics
+import test_class
+from scipy.integrate import quad
+
+
+def mle_direct_covariance(test_parameters):
+    #test_parameters is of type test_class.TestParameters
+    raise NotImplementedError
