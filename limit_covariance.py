aa1da2657c077046e207369c80d8d4bf295a6697
Alexander Heckett <aheckett@gmail.com>
2022-07-05 21:24:54 -0400

    Wrote scorematching_limit_covariance function

--- limit_covariance.py
+++ limit_covariance.py
@@ -61,9 +61,9 @@ def mle_limit_covariance(test_parameters):
     for row_suffstat in test_parameters.suffstats:
         E_FxFxT_row = []
         for column_suffstat in test_parameters.suffstats:
-            def product_suffstat(x):
+            def suffstats_product(x):
                 return row_suffstat.zeroth_derivative(x) * column_suffstat.zeroth_derivative(x)
-            entry = expectation(product_suffstat, test_parameters, Z)
+            entry = expectation(suffstats_product, test_parameters, Z)
             E_FxFxT_row.append(entry)
         E_FxFxT.append(E_FxFxT_row)
     E_FxFxT = np.array(E_FxFxT)
@@ -77,4 +77,58 @@ def mle_limit_covariance(test_parameters):
 
 
 def scorematching_limit_covariance(test_parameters):
-    #COME BACK
+    #test_parameters is of type test_class.TestParameters.
+
+    #First, compute the partition function.
+    Z = partition(test_parameters)
+
+    #Let's now compute the "outer" matrix
+    E_JFxJFxT = []
+    for row_suffstat in test_parameters.suffstats:
+        E_JFxJFxT_row = []
+        for column_suffstat in test_parameters.suffstats:
+            def first_derivatives_product(x):
+                return (-row_suffstat.first_derivative(x)) * (-column_suffstat.first_derivative(x))
+            entry = expectation(first_derivatives_product, test_parameters, Z)
+            E_JFxJFxT_row.append(entry)
+        E_JFxJFxT.append(E_JFxJFxT_row)
+    E_JFxJFxT = np.array(E_JFxJFxT)
+    outer_matrix = np.linalg.inv(E_JFxJFxT)
+
+    #Now, let's compute the "inner" matrix
+    #We'll start by defining some of the functions involved.
+    def A(x):
+        JFx = -ss.first_derivatives(test_parameters.suffstats, np.array([x]))
+        JFx_JFxT = np.outer(JFx, JFx)
+        JFx_JFxT_theta = np.dot(JFx_JFxT, -test_parameters.theta_star)
+        Delta_F = -ss.second_derivatives(test_parameters.suffstats, np.array([x]))
+        return JFx_JFxT_theta - Delta_F
+
+    #This is computationally stupid because we compute all of A
+    #each time we ask for each component, but it's a bit nicer to think about.
+    As = [lambda x: A(x)[i] for i in range(len(test_parameters.suffstats))]
+
+    #We can now duplicate the code from the MLE case to get E[A(x) A(x)^T] - E[A(x)] E[A(x)^T]
+    #i.e. Sigma_A
+
+    E_Ax = [expectation(As_i, test_parameters, Z) for As_i in As]
+    E_Ax = np.array(E_Ax)
+    E_Ax_E_AxT = np.outer(E_Ax, E_Ax)
+
+    E_AxAxT = []
+    for As_i in As:
+        E_AxAxT_row = []
+        for As_j in As:
+            def As_i_As_j_product(x):
+                return As_i(x) * As_j(x)
+            entry = expectation(As_i_As_j_product, test_parameters, Z)
+            E_AxAxT_row.append(entry)
+        E_AxAxT.append(E_AxAxT_row)
+    E_AxAxT = np.array(E_AxAxT)
+
+    Sigma_A = E_AxAxT - E_Ax_E_AxT
+
+    #Finally we can compute our answer:
+    #(Note: this is actually computing the covariance matrix for -theta,
+    #but that's the same as the covariance matrix for theta.)
+    return np.matmul(outer_matrix, np.matmul(Sigma_A, outer_matrix)) / test_parameters.n
